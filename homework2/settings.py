97284de5c7c0798847ec8e9aee4a74f970df4b6d
goldikfish <112796030+goldikfish@users.noreply.github.com>
2023-04-21 20:53:53 +0800

    Create settings.py

--- homework2/settings.py
+++ homework2/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'hw2db',
     'USER': 'root',
-    'PASSWORD': '123456',
+    'PASSWORD': '',
     'HOST': 'localhost',
     'PORT': '3306',
     }   
